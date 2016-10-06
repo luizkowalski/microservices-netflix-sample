bca1cd5694f331c1a1739d91eb13d3c8e05fa981
Luiz Eduardo <luizeduardokowalski@gmail.com>
2016-10-06 23:55:55 +0200

    Config

--- edge/src/main/java/com/inkdroplabs/EdgeApplication.java
+++ edge/src/main/java/com/inkdroplabs/EdgeApplication.java
@@ -2,12 +2,10 @@ package com.inkdroplabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulServer
-@EnableDiscoveryClient
+@EnableZuulProxy
 public class EdgeApplication {
 
 	public static void main(String[] args) {
