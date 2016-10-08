e9b47a5b50770fa98a9ea950a6033e9fb365d6ed
Luiz Eduardo <luizeduardokowalski@gmail.com>
2016-10-08 16:09:00 +0200

    Edge server working

--- build-all.sh
+++ build-all.sh
@@ -4,6 +4,7 @@ set -e
 
 cd eureka-server; mvn clean package -DskipTests=true; cd -
 cd contacts; mvn clean package -DskipTests=true; cd -
-cd api; mvn clean package -DskipTests=true; cd -
+cd edge; mvn clean package -DskipTests=true; cd -
+# cd api; mvn clean package -DskipTests=true; cd -
 
 find . -name *SNAPSHOT.jar -exec du -h {} \;
