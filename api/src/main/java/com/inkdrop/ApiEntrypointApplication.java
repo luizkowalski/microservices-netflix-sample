28ee29b67499ad338fb4c9f1aa3c021330e4ab31
Luiz Eduardo <luizeduardokowalski@gmail.com>
2016-10-22 21:26:42 +0200

    WIP

--- api/src/main/java/com/inkdrop/ApiEntrypointApplication.java
+++ api/src/main/java/com/inkdrop/ApiEntrypointApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
-@EnableHystrixDashboard
 public class ApiEntrypointApplication {
 
 	public static void main(String[] args) {
--- hystrix/src/main/java/com/inkdroplabs/HystrixDashboardApplication.java
+++ hystrix/src/main/java/com/inkdroplabs/HystrixDashboardApplication.java
@@ -2,10 +2,14 @@ package com.inkdroplabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableTurbine
+@EnableEurekaClient
 public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
